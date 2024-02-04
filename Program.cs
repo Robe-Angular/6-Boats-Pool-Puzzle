4fccd3a550b1a4ad901f02690b26fffbb1ac6fd6
robe <robe@robe.com>
2024-02-03 20:27:30 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_Boats_Pool_Puzzle
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Maines()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Sailboat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Boats_Pool_Puzzle
+{
+    class Sailboat : Boat
+    {
+        public override string move()
+        {
+            return "hoist sail";
+        }
+    }
+}
